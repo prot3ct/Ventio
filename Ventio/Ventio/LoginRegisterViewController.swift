eb3c1ef3cb8fec72cfaa5a56d8cd2795629dc30b
prot3ct <prot3ct@prot3cts-iMac.local>
2017-04-01 15:02:31 +0300

    Init

--- Ventio/Ventio/LoginRegisterViewController.swift
+++ Ventio/Ventio/LoginRegisterViewController.swift
@@ -1,15 +1,11 @@
-//
-//  LoginRegisterViewController.swift
-//  Ventio
-//
-//  Created by prot3ct on 4/1/17.
-//  Copyright © 2017 Georgi Karaboichev. All rights reserved.
-//
-
 import UIKit
 
 class LoginRegisterViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //var userData: UserDataProtocol!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +17,10 @@ class LoginRegisterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onLoginClicked(_ sender: UIButton) {
+        print("Clciked")
+    }
+
 
     /*
     // MARK: - Navigation
